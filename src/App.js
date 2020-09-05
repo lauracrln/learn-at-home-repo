83cd0b104f2c57bdc54599c678c194480a047011
lauracrln <laura.kurniawan@dana.id>
2020-09-05 14:50:55 +0700

    tambahin state and event

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 // import './App.css';
 
 
+const Post = props => (
+  <img src="test" />
+)
   
-const Profile = (props) => (
-
+const Profile = (props) => {
+  const [isFollowed, setIsFollowed] = useState(false);
+  //bakal balikin isfollowed(statenya) sama setter (setisfollowed), state itu read only, makanya kalo mau rubah value di setternya
+  return (
     <React.Fragment>
-    <img src="https://via.placeholder.com/300" alt="profile picture" />
-      <div>@[username]</div>
-      <button>Follow</button>
+      <img src="https://via.placeholder.com/300" alt="profile picture" />
+      <div>@{props.username}</div>
+      <button onClick={() => setIsFollowed(!isFollowed)}>{isFollowed ? "Unfollow" : "Follow"}</button>
       <div>Post</div>
       <div>Followers</div>
       <div>Following</div>
       <div>Bio</div>
     </React.Fragment>
-  );
+    );
+  };
 
 
 export default function App() {
