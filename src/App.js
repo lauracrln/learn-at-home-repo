4cbccb7a58ba1a6dd1301dd3dbcfab88f161ca55
lauracrln <laura.kurniawan@dana.id>
2020-09-05 15:10:09 +0700

    add loop

--- src/App.js
+++ src/App.js
@@ -3,22 +3,32 @@ import logo from './logo.svg';
 // import './App.css';
 
 
-const Post = props => (
-  <img src="test" />
-)
+const Post = ({ src }) => <img src={src} alt="profile picture" />;
   
 const Profile = (props) => {
+  const {username} = props;
   const [isFollowed, setIsFollowed] = useState(false);
-  //bakal balikin isfollowed(statenya) sama setter (setisfollowed), state itu read only, makanya kalo mau rubah value di setternya
+  //bakal balikin isfollowed(statenya) sama setter (setisfollowed), state itu read only, makanya kalo mau rubah value di setternya. 
+  //setIsFollowed itufunction untuk mengubah statenya 
+  const posts = [...Array(0)];
+  const postCount = 0;
+
   return (
     <React.Fragment>
-      <img src="https://via.placeholder.com/300" alt="profile picture" />
+      <img src="https://via.placeholder.com/100" alt="profile picture" />
       <div>@{props.username}</div>
       <button onClick={() => setIsFollowed(!isFollowed)}>{isFollowed ? "Unfollow" : "Follow"}</button>
-      <div>Post</div>
+      <div>{postCount}Post</div>
       <div>Followers</div>
       <div>Following</div>
       <div>Bio</div>
+      {postCount === 0 ? (
+        <div>No Post</div>
+      ) : (
+        posts.map((_,idx) => (
+          <Post src ="https://via.placeholder.com/100" key={idx} />
+        ))
+      )}
     </React.Fragment>
     );
   };
